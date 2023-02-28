55bf08951e55ff4c7dd48bb024bf69755ab880e9
yyf <594371124@qq.com>
2023-03-01 02:44:07 +0800

    一次次的修改

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { presetIcons, presetAttributify, presetUno} from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
