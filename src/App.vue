d8ab10e75c032b0fa6992f15d5e43c4a0730c80e
yyf <594371124@qq.com>
2023-03-16 17:41:15 +0800

    header部分

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,31 @@
 </template>
 <script lang='ts' setup>
 </script>
-<style scoped></style>
+<style>
+html,body,#app {
+    min-width: 1280px;
+    height: 100%;
+    background: url('./assets/images/421.jpg') no-repeat  scroll ;
+    background-size: cover;
+    overflow: auto;
+}
+::-webkit-scrollbar {
+    width: 1px;
+    height: 3px;
+    border-radius: 1px;
+    background-color: #F5F5F5;
+}
+::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    border-radius: 10px;
+    background-image: -webkit-gradient(linear,left bottom,left top,color-stop(0.44,rgb(255, 255, 255)),color-stop(0.72,rgb(253, 252, 251)),color-stop(0.86,rgb(252, 251, 250)));
+    transition: 0.3s ease-in-out;
+}
+::-webkit-scrollbar-track {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
+}
+
+</style>
--- src/layout/header/index.vue
+++ src/layout/header/index.vue
@@ -1,12 +1,27 @@
 <template>
-<div class="header">头部区域</div>
+<div class="header shadow-md" >
+    <logoIcon class="p-t-1 animate__animated animate__heartBeat"></logoIcon>
+    <button @click="toggle">全屏</button> 
+    <el-button>登录/注册</el-button>
+</div>
+
 </template>
 <script lang='ts' setup>
 import { ref, reactive } from 'vue'
+import 'animate.css'
+import logoIcon from './logoIcon.vue';
+import { useFullscreen } from '@vueuse/core'
+const { toggle } = useFullscreen()
+
 
 </script>
 <style scoped>
-.el-header {
-    color: red;
+.header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.el-button {
+    color: #FF8C00;
 }
 </style>
